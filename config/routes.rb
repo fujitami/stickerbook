3e36e37ec042e0128492f26a8a091c59e9621623
fujitami <midori.fujita.0423@gmail.com>
2025-10-29 20:02:38 +0900

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post   "/signup", to: "users#create"
   post   "/login",  to: "sessions#create"
   delete "/logout", to: "sessions#destroy", defaults: { format: :json }
-  
+
   resources :stickers, only: [ :index, :create, :show ]
   resource :me, only: [ :show ], controller: :me do
     resources :ownerships, only: [ :index ], controller: "me/ownerships"
