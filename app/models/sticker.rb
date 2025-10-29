179e6afbe3fa85c7c4facf49bebdc055c8854a64
fujitami <midori.fujita.0423@gmail.com>
2025-10-29 19:54:48 +0900

    add ownership

--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,6 +1,7 @@
 class Sticker < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :ownerships, dependent: :destroy
 
   validate :acceptable_image
   def acceptable_image
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   post   "/signup", to: "users#create"
   post   "/login",  to: "sessions#create"
   delete "/logout", to: "sessions#destroy", defaults: { format: :json }
-
-  resource :me, only: [ :show ], controller: :me
-
+  
   resources :stickers, only: [ :index, :create, :show ]
+  resource :me, only: [ :show ], controller: :me do
+    resources :ownerships, only: [ :index ], controller: "me/ownerships"
+  end
+  resources :ownerships, only: [ :create, :destroy ]
 end
